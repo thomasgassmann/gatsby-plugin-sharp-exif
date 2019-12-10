28d106b75d7106f927e2947efd7eba28f8d83ae3
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-12-10 08:55:03 +0100

    read gps and other metadata separately, expose full exif data through "raw"

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,20 +1,56 @@
 import { read } from 'fast-exif';
 
-export function onCreateNode({ node, getNode, actions }) {
-    const { createNodeField } = actions;
-    if (node.internal.type === 'ImageSharp') {
-        const parent = getNode(node.parent);
+function convertDMSToDD(dms, positiveDirection) {
+  const factor = 60;
+  const res = dms
+    .map((item, i) => {
+      const divisor = factor * i;
+      return divisor === 0 ? item : item / divisor;
+    })
+    .reduce((a, b) => a + b);
+  return positiveDirection ? res : -res;
+}
 
-        read(parent.absolutePath)
-            .then((exifData) => {
-                createNodeField({
-                    node,
-                    name: 'exif',
-                    value: {
-                        ...exifData
-                    }
-                });
-            })
-            .catch((err) => console.error(err));
-    }
+export function onCreateNode({ node, getNode, actions }) {
+  const { createNodeField } = actions;
+  if (node.internal.type === 'ImageSharp') {
+    const parent = getNode(node.parent);
+
+    read(parent.absolutePath)
+      .then(exifData => {
+        if (!exifData) {
+          console.warn(`Could not read exif of ${parent.absolutePath}`);
+          return;
+        }
+
+        const gps = { longitude: null, latitude: null };
+        if (
+          exifData.gps &&
+          exifData.gps.GPSLongitude &&
+          exifData.gps.GPSLatitude
+        ) {
+          gps.longitude = convertDMSToDD(
+            exifData.gps.GPSLongitude,
+            exifData.gps.GPSLongitudeRef === 'E'
+          );
+          gps.latitude = convertDMSToDD(
+            exifData.gps.GPSLatitude,
+            exifData.gps.GPSLatitudeRef === 'N'
+          );
+        }
+
+        createNodeField({
+          node,
+          name: 'exif',
+          value: {
+            gps,
+            meta: {
+              dateTaken: exifData.exif && exifData.exif.DateTimeOriginal
+            },
+            raw: exifData
+          }
+        });
+      })
+      .catch(err => console.error(err));
+  }
 }
