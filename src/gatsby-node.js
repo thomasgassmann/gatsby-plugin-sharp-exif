cf066334055e9e801c4548c5824b106a53fef1dc
Zhou Hua <zhou--hua@163.com>
2024-02-28 19:30:20 +0800

    feat: add plugin parameters (includes and excludes)

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,24 +1,83 @@
 import { read } from 'fast-exif';
+import fg from 'fast-glob';
+import { once } from 'lodash';
 import { transformExifToNodeData } from './common';
+import { cwd } from 'process';
 
-export function onCreateNode({ node, getNode, actions }) {
+export function pluginOptionsSchema({ Joi }) {
+  return Joi.object({
+    includes: Joi.array().items(Joi.string()),
+    excludes: Joi.array().items(Joi.string()),
+  });
+}
+
+let includeImageListAsync = null;
+let excludeImageListAsync = null;
+
+function globQuery(globPatterns) {
+  if (globPatterns?.length) {
+    return fg.glob(globPatterns, {
+      absolute: true,
+      onlyFiles: true,
+      unique: true,
+      baseNameMatch: true,
+      dot: true,
+    });
+  } else {
+    return Promise.resolve([]);
+  }
+}
+
+const getAllImages = once((includes, excludes) => {
+  includeImageListAsync = globQuery(includes);
+  excludeImageListAsync = globQuery(excludes);
+});
+
+async function processExif(absolutePath, createNodeField, node) {
+  try {
+    const exifData = await read(absolutePath);
+
+    if (!exifData) {
+      console.warn(`Could not read exif of ${absolutePath}`);
+      return;
+    }
+
+    createNodeField({
+      node,
+      name: 'exif',
+      value: transformExifToNodeData(exifData),
+    });
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function onCreateNode(
+  { node, getNode, actions },
+  { includes = [], excludes = [] }
+) {
   const { createNodeField } = actions;
   if (node.internal.type === 'ImageSharp') {
     const parent = getNode(node.parent);
+    try {
+      if (includes?.length || excludes?.length) {
+        getAllImages(includes, excludes);
 
-    read(parent.absolutePath)
-      .then(exifData => {
-        if (!exifData) {
-          console.warn(`Could not read exif of ${parent.absolutePath}`);
-          return;
+        const includeImageList = await includeImageListAsync;
+        const excludeImageList = await excludeImageListAsync;
+
+        if (
+          includeImageList.includes(parent.absolutePath) &&
+          !excludeImageList.includes(parent.absolutePath)
+        ) {
+          await processExif(parent.absolutePath, createNodeField, node);
         }
-
-        createNodeField({
-          node,
-          name: 'exif',
-          value: transformExifToNodeData(exifData)
-        });
-      })
-      .catch(err => console.error(err));
+      } else {
+        await processExif(parent.absolutePath, createNodeField, node);
+      }
+    } catch (err) {
+      console.warn(`Could not read exif of ${parent.absolutePath}`);
+      console.error(err);
+    }
   }
 }
